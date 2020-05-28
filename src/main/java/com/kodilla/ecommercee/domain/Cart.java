d7023bdbd43b693cba4f7f4d08d271e239b28e55
leetdco2@gmail.com <leetdco2@gmail.com>
2020-05-28 18:51:23 +0200

    JDP200503-3 Create Class Cart, CartDto and CartController

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,54 @@
+package com.kodilla.ecommercee.domain;
+
+import javax.persistence.*;
+
+@Entity(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "groupId")
+    private String groupId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public Cart() {
+    }
+
+    public Cart(String name, String description, double price, String groupId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.groupId = groupId;
+    }
+
+
+}
