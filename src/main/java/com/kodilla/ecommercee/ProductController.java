fd22670cf53a17edb0842b6f86715a4a4a2f532a
jakubG78 <jakub.gora@wp.pl>
2020-05-28 19:38:41 +0200

    Modified package structure, created ProductDto, modyfied ProductController

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++
@@ -1,5 +0,0 @@
-package com.kodilla.ecommercee;
-
-public class ProductController {
-    //Start of task JDP200503-1
-}
---
+++ src/main/java/com/kodilla/ecommercee/controler/OrderController.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee.controler;
+
+public class OrderController {
+}
