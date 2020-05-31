62c65a2cbd2c093e497dd4e55df6d54167d4d4aa
leetdco2@gmail.com <leetdco2@gmail.com>
2020-05-31 16:46:25 +0200

    JDP200503-14 - created CART entity

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,73 @@
+package com.kodilla.ecommercee.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="CART")
+public class Cart {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String groupId;
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="ID", unique = true)
+    public int getId(){
+        return id;
+    }
+    @Column(name="NAME")
+    public String getName()
+    {return name;}
+
+    @Column(name="DESCRIPTION")
+    public String getDescription(){
+        return description;
+    }
+
+    @Column(name = "PRICE")
+    public double getPrice(){
+        return price;
+    }
+
+    @Column(name = "GROUP_ID")
+    public String getGroupId(){
+        return groupId;
+    }
+
+    public Cart() {
+    }
+
+    public Cart(int id,String name, String description,double price,String groupId)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.groupId = groupId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+}
