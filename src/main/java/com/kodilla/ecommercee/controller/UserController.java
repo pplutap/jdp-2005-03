27ea71c09b9cfb2a214706aa7ec1afdd91864499
B3fromSuffering <laczny.maciej@gmail.com>
2020-06-01 11:18:49 +0200

    poprawa: pola userDto, metoda getKey, konflikty

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ecommercee/user")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public void createUser(UserDto userDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    public void blockUser(Long userId) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "generateKey")
+    public Long generateKey(UserDto userDto) {
+        return 1234567L;
+    }
 }
