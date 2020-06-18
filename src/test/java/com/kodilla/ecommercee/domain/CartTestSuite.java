68d4968e44c44bf8d4905e50cf9a5b7131c64be2
jakubG78 <jakub.gora@wp.pl>
2020-06-18 21:54:45 +0200

    Add CartTestSuite, modified CartService

--- src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
@@ -1,31 +1,77 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.service.CartService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class CartTestSuite {
 
+    @Autowired
+    CartService cartService;
+
     @Test
-    public void shouldCreateCart(){
+    public void shouldCreateAndDeleteCart() {
         //Given
-        Cart cart = new Cart("testCart","Cart added for test", 550.00);
+        Cart cart = new Cart("testCart", "Cart added for test", 550.00);
 
         //When
-        String cartName = cart.getName();
-        String cartDescriptiom = cart.getDescription();
-        double totalCartPrice = cart.getPrice();
+        Cart savedCart = cartService.save(cart);
 
         //Then
-        Assert.assertEquals("testCart", cartName);
-        Assert.assertEquals("Cart added for test", cartDescriptiom);
-        Assert.assertEquals(550.00, totalCartPrice,0);
+        Assert.assertNotNull(savedCart);
+        Assert.assertNotNull(cart.getId());
+
+        //CleanUp
+        cartService.delete(cart);
     }
 
+    @Test
+    public void shouldCreateAndDeleteCarts() {
+        //Given
+        List<Cart> cartList = new ArrayList<>();
+        Cart cart1 = new Cart("testCart#1", "Cart#1 added for test", 550.00);
+        Cart cart2 = new Cart("testCart#2", "Cart#2 added for test", 921.15);
+        cartList.add(cart1);
+        cartList.add(cart2);
 
+        //When
+        cartService.saveAll(cartList);
+        //Then
+        Assert.assertNotNull(cartService.findById(cart1.getId()));
+        Assert.assertNotNull(cartService.findById(cart2.getId()));
+
+        //CleanUp
+        cartService.delete(cart1);
+        cartService.delete(cart2);
+    }
+
+    @Test
+    public void shouldFindAndSaveModifiedCart() {
+        //Given
+        Cart cart = new Cart("testCart", "Cart added for test", 550.00);
+
+        //When
+        cartService.save(cart);
+        String originalCartName = cart.getName();
+        cart.setName("Modified test Cart");
+        cartService.save(cart);
+        Cart modifiedCart = cartService.findById(cart.getId());
+
+        //Then
+        Assert.assertNotEquals(originalCartName, modifiedCart.getName());
+        Assert.assertEquals("Modified test Cart", modifiedCart.getName());
+
+        //CleanUp
+        cartService.delete(cart);
+    }
 
 }
